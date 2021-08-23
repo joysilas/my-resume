6ff804a147a86b610adf76dce0165765e980c92b
Joy <silasjoy22@gmail.com>
2021-08-23 19:42:49 +0100

    portfolio page

--- send-contact-mail.php
+++ send-contact-mail.php
@@ -1,17 +1,47 @@
 <?php
-if(!empty($_POST['send'])) {
-    global $db;
+if (!empty($_POST['send'])) {
+	global $db;
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$subject = $_POST['subject'];
 	$message_user = $_POST['message'];
 
-	$toEmail = "silasjoy22@gmail.com";
-	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
-	if(mail($toEmail, $subject, $message_user, $mailHeaders)) {
-	    $message = "Your contact information is received successfully.";
-	    $type = "success";
-	}
+	$message = "Your contact information is received successfully.";
+	$type = "success";
+	$count = 0;
+	$report = "Hi, " . $name . " thank you for contacting me";
+	echo Alert();
 }
+function Alert()
+{
+	global $report, $count;
+
+	echo '<script>setTimeout(function() {
+            						$("#refresh").fadeOut(3000);
+        							}, 3000);
+    						  </script>';
+	if ($count > 0) {
+		echo '
+            	<div id="refresh"  class="alert bg-danger" style="position:fixed; top:10px; right:10px; z-index:10000; width: auto;">
+				<i class="icon fa fa-ban text-white"> &nbsp;' . $report . '</i></div>';
+	} else {
+		echo '<div id="refresh"  class="alert bg-success" style="position:fixed; top:10px; right:10px; z-index:10000; width: auto;">
+				<i class="icon fa fa-check text-white"> &nbsp;' . $report . '</i></div>';
+	}
+	return;
+}
+
+if (isset($report)) {
+	echo "<script>
+					$(function() {
+						$('body').on('click', '#submit', function() {
+							document.body.innerHTML('Thank you for contacting me!')
+						})
+					})
+				</script>";
+}
+
+
+
+
 require_once "index.php";
-?>
